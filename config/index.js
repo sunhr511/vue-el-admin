0b75a81acf89ecc13e193e772f05de8961653627
朱锡文 <574715866@qq.com>
2018-05-15 15:38:10 +0800

    d

--- config/index.js
+++ config/index.js
@@ -57,14 +57,14 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist2/admin.ftl'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist2/admin/'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
 
     // you can set by youself according to actual condition
-    assetsPublicPath: '../../admin/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
